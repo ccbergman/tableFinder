3ea95e5962247ad0a9c36129efbe9bbd31cb789e
Chelsea Bergman <chelseacbergman@gmail.com>
2017-10-31 16:41:42 -0400

    form innerworkings

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
     console.log("Hi");
 
+    var tableIsReserved;
+
     $(".table").on("click", function () {
-        $("#reserveForm").css("display", "block");
+        if ($(this).hasClass("available")) {
+            $("#reserveForm").css("display", "block");
+            tableIsReserved = $(this);
+        }
     });
 
+    $("#saveReservation").on("click", function () {
+        if ($(".table.available")) {
+            $(tableIsReserved).removeClass('available').addClass('reserved');
+            $(tableIsReserved).css("cursor", "not-allowed");
+            $(tableIsReserved).css("color", "#EEE");
+            $(tableIsReserved).css("background-color", "#ADAAAA");
+            $("#reserveForm").css("display", "none");
+        }
+    });
 
     $(".exitIcon").on("click", function () {
         $("#reserveForm").css("display", "none");
     });
-
-
-    
 });
