0b5acfc623156e8620f67130d630ec0ca001a571
Chelsea Bergman <chelseacbergman@gmail.com>
2017-10-31 20:57:48 -0400

    working array

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ $(document).ready(function () {
     console.log("Hi");
 
     var tableIsReserved;
+    var tableData = [];
+    for (var i = 1; i < 11; i++) {
+        var seating = { name: "", count: 0 };
+        tableData.push(seating);
+    }
 
     $(".table").on("click", function () {
         if ($(this).hasClass("available")) {
-            $("#reserveForm").slideDown().css("display", "block");
+            $("#reserveForm").slideDown("slow").css("display", "block");
+            $("#guestName").val("");
+            $("#guestPartySize").val("");
             tableIsReserved = $(this);
+            var tablenum = $(this).attr("data-tablenum");
+            $("#tableNumber").text("Table: " + tablenum);
         }
     });
 
@@ -16,11 +25,18 @@ $(document).ready(function () {
             $(tableIsReserved).css("cursor", "not-allowed");
             $(tableIsReserved).css("color", "#EEE");
             $(tableIsReserved).css("background-color", "#ADAAAA");
-            $("#reserveForm").css("display", "none");
+
+            var tablenum = $(tableIsReserved).attr("data-tablenum");
+
+            var gName = $("#guestName").val();
+            var gCount = $("#guestPartySize").val();
+            tableData.splice(tablenum, 1, { name: gName, count: gCount });
+
+            $("#reserveForm").fadeOut("slow");
         }
     });
 
     $(".exitIcon").on("click", function () {
-        $("#reserveForm").css("display", "none");
+        $("#reserveForm").fadeOut("slow");
     });
 });
